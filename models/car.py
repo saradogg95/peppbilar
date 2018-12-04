30b9afdc67c3be14b5a5017319c4705251c3caf9
Unnar Fridrik Sigurdsson <sveppagreifinn@gmail.com>
2018-12-04 19:39:57 +0000

    Snyrti aðeins til

--- models/car.py
+++ models/car.py
@@ -1,14 +1,3 @@
-"""
-id: int
-type: string
-category: string
-manufacturer: string
-registration_date: date
-plate_number: string
-mileage: int
-is_available: bool
-"""
-
 class Car:
     def __init__(self, regNum=0, make="", category="", manufacturer="", registration_date="", mileage=0, is_available=True):
         self.__regNum = regNum
@@ -19,7 +8,7 @@ class Car:
         self.__mileage = mileage
         self.__availability = is_available
     
-    def __str__(self):
+    def __str__(self): #gera þetta print aðeins flottara
         return "{} {} {} {} {} {} {}".format(self.__regNum, self.__make, self.__category, self.__manufacturer, self.__registration_date, self.__mileage, self.__availability)
 
     def get_regNum(self):
