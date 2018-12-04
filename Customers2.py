c04f5002ffe84aa775e881d81c5f72242eea3b04
Árni Fannar <arnis18@ru.is>
2018-12-04 20:03:33 +0000

    Models for customers

--- Customers2.py
+++ Customers2.py
@@ -1,26 +1,31 @@
-import csv
+class Customers():
+    def __init__(self, customer_ID=0, kt="", first_names="", surname="", country="", password=""):
+        self.customer_ID = customer_ID
+        self.kt = kt
+        self.first_names = first_names
+        self.surname = surname
+        self.country = country
+        self.password = password
 
-class Customer():
-    def __init__(self, customer_ID, kt, first_name, surname, country, passport):
-        self.customer_ID = 
-        self.kt = 
-        self.first_name = 
-        self.surname = 
-        self.country =
-        self.passport = 
+    def __str__(self):
+        return "{} {} {} {} {} {}".format(self.customer_ID, self.kt, self.first_names, self.surnames, self.country, self.passport)
 
-def main():
+
+    def get_customer_ID(self):
+        return self.customer_ID
     
-    with open('Customer_grunnur.csv')
+    def get_kt(self):
+        return self.kt
 
-    Kennitala	Eiginnafn/eiginnöfn	Eftirnafn	Ríkisfang	Vegabréfsnúmer
-
-"""Ná í lista yfir viðskiptavini."""
-
-
-
-"""Finna viðskiptavin."""
-
-"""Bæta við viðskiptavini."""
-
-"""Eyða viðskiptavini."""
+    def get_first_names(self):
+        return self.first_names
+    
+    def get_surname(self):
+        return self.surname
+    
+    def get_country(self):
+        return self.country
+    
+    def get_passport(self):
+        return self.passport
+    
