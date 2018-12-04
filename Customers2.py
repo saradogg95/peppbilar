357a85108cfc4a0d0a4955b4df665d75ac703fa3
Árni Fannar <arnis18@ru.is>
2018-12-04 19:49:30 +0000

    Model file for customers.

---
+++ Customers2.py
@@ -0,0 +1,26 @@
+import csv
+
+class Customer():
+    def __init__(self, customer_ID, kt, first_name, surname, country, passport):
+        self.customer_ID = 
+        self.kt = 
+        self.first_name = 
+        self.surname = 
+        self.country =
+        self.passport = 
+
+def main():
+    
+    with open('Customer_grunnur.csv')
+
+    Kennitala	Eiginnafn/eiginnöfn	Eftirnafn	Ríkisfang	Vegabréfsnúmer
+
+"""Ná í lista yfir viðskiptavini."""
+
+
+
+"""Finna viðskiptavin."""
+
+"""Bæta við viðskiptavini."""
+
+"""Eyða viðskiptavini."""
