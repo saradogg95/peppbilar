97455fe56d48710c7ad8a69d70fc25912575878c
Valtyrs <valtyr321@gmail.com>
2018-12-04 15:10:36 +0000

    Version 1

--- models/user.py
+++ models/user.py
@@ -1,8 +1,30 @@
 
 
-class User:
+class Employee:
 
-    def __init__(self):
+    def __init__(self, username, password, name, address, age, admin=False):
+        
+        self.__username = username
+        self.__password = password
+        self.__name = name
+        self.__address = address
+        self.__age = self.age_check(age)
 
-        pass
+        self.__admin = admin
 
+    def age_check(self, age):
+        done = False
+        while not done:
+            try:
+                if int(age) >= 0:
+                    return age
+            except:
+                age = input("Please input a number >= 0 for age.")
+
+    def __str__(self):
+        return "{}, {}, {}, {}, {}, {}".format(self.__username, self.__password, self.__name, 
+                                    self.__address, self.__age, self.__admin)
+
+
+abi = Employee("doctor", 123, "Valtýr Oddsson", "Sævangur 22", -1, admin=True)
+print(abi)
