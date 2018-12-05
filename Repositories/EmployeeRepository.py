7139c33b197bc2a121c5fbfe3a310d3d30530c09
unknown <throstur15@ru.is>
2018-12-05 09:48:46 +0000

    Bætti við skránnum EmployeeRepository.py

--- Repositories/EmployeeRepository.py
+++ Repositories/EmployeeRepository.py
@@ -1,25 +1,36 @@
-from models.Video import Video
+import models.Empolyee from Employee
 
-class VideoRepository:
-    
+
+
+
+class EmployeeRepository:
     def __init__(self):
-        self.__videos = []
+        self.__employee = []
 
-    def add_video(self, video):
-        # first add to file then to private list
-        with open("./data/videos.txt", "a+") as videos_file:
-            title = video.get_title()
-            genre = video.get_genre()
-            length = video.get_length()
-            videos_file.write("{},{},{}\n".format(title, genre, length))
+    def __str__(self):
+        return "{}".format(self.__employee)
 
-    def get_videos(self):
-        if self.__videos == []:
-            with open("./data/videos.txt", "r") as video_file:
+    def __repr__(self):
+            return self.__str__()
+    
+    def add_Employee(self, employee):
+        with open("./data/employee.txt", "a+") as employee_file:
+            name = employees.get_name()
+            age = employees.get_age()
+            address = employees.get_address()
+            username = employees.get_user_name()
+            password = employees.get_password()
+            admin = employees.get_admin()
+            employee_file.write("{},{},{}\n".format(name, age, address, username, password, admin))
+    
+    def get_employees(self):
+        if self.__employee == []:
+            with open("./data/employee.txt", "r") as video_file:
                 for line in video_file.readlines():
-                    title, genre, length = line.split(",")
-                    new_video = Video(title, genre, length)
-                    self.__videos.append(new_video)    
+                    name, age, address, username, password, admin = line.split(",")
+                    new_employee = Employee(username, password, name, address, age, admin=0)
+                    self.__employee.append(new_video)    
         
-        return self.__videos
+        return self.__employee
+    
 
