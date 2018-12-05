0cba3a0eb63a88cb05214546615a549f9ae20ec8
Unnar Fridrik Sigurdsson <sveppagreifinn@gmail.com>
2018-12-05 22:26:21 +0000

    Working, probably missing some checks

--- services/CarServices.py
+++ services/CarServices.py
@@ -1,20 +1,29 @@
+from models.Car import Car
 from repositories.CarRepo import CarRepository
 
 
 class CarServices():
     def __init__(self):
         self.__repository = CarRepository()
+        self.__cars = []
 
     def get_car(self, id):
-        """ tekur inn bílnúmer og biður gagnagrunninn á að senda það. gagnagrunnurinn skilar ordered dict með bíl sem finnst en annars None. 
-        Þetta fall vinnur úr gögnunum og skilar streng með bílnum en annars """
+        """ tekur inn bílnúmer og biður gagnagrunninn um það. gagnagrunnurinn skilar ordered dict með bíl sem finnst en annars None. 
+        Þetta fall vinnur úr gögnunum og skilar streng með bílnum en annars streng að bíllinn finnist ekki"""
+        get_car = self.__repository.get_car(id)
+        if get_car != None:
+            return get_car
+        return "No car with registration number {} found".format(id)
 
     def add_car(self, car):
-        pass
+        """ tekur eintak af bíl og sendir áfram í gagnagrunninn til skrifunar """
+        self.__repository.add_car(car)
 
     def get_all_cars(self):
-        pass
+        """ biður um að fá lista yfir alla bíla senda úr gagnagrunninum og skilar í lista """
+        self.__cars = self.__repository.get_all_cars()
+        return self.__cars
 
     def get_available_cars(self):
-        pass
-
+        self.__cars = self.__repository.get_available_cars()
+        return self.__cars
