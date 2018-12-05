5e570a8134e843632b7fd05bbb5a0c33d9ac1a06
Árni Fannar <arnis18@ru.is>
2018-12-05 16:40:30 +0000

    work in progress.

--- Repositories/customersRepo_V2.py
+++ Repositories/customersRepo_V2.py
@@ -6,7 +6,7 @@ class customerRepository:
     def __init__(self):
         self.customers = []
 
-    def get_customer(self,identity_number):
+    def add_customer(self,identity_number):
         with open("Customer_grunnur_TBD.csv", "r") as customer_db:
             customer_ID = Customers.get_customer_ID
             identity_number = Customers.get_identity_number
@@ -14,6 +14,8 @@ class customerRepository:
             surname = Customers.get_surname
             citizenship = Customers.get_citizenship
             passport_ID = Customers.get_passport_ID
+            customer_db.write("{},{},{},{},{}").format(customer_ID, first_names, surname, citizenship,passport_ID)
+
 
             csv_dict = csv.DictReader(customer_db)
             for line in csv_dict:
@@ -21,12 +23,11 @@ class customerRepository:
                     return line
         return None
     
-    def add_customer(self, identity_number):
+    def get_customer(self, identity_number):
         with open("Customer_grunnur_TBD.csv", "a+") as customer_db:
-            identity_number = Customers.get_identity_number()
-            first_names = Customers.get_first_names()
-            surname = Customers.get_surname()
-            citizenship = Customers.get_citizenship()
-            customer_ID = Customers.get_customer_ID()
-            customer_db.write("{},{},{},{}").format(customer_ID, first_names, surname, citizenship,)
+            csv_dict = csv.DictReader(customer_db)
+            for line in csv_dict:
+                if line["kennitala"] == self.identity_number:
+                    return line
+        return None
 
