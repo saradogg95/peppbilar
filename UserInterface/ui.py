48f3ab580b93d8e8ddc052ada62ecd71bb62dc5a
Valtyrs <valtyr321@gmail.com>
2018-12-05 18:46:23 +0000

    Version 4

--- UserInterface/ui.py
+++ UserInterface/ui.py
@@ -12,13 +12,23 @@ class UserInterface:
 
     def main_menu(self):
         
-        action = ""
+        location_flag = "Main menu"
+
+   
 
         while action != "q":
-            self.print_choices(action)
+
+            self.print_choices(location_flag)
             action = self.get_action()
+
+
             if action == 1:
-                pass
+                location_flag = "Order menu"
+                while action != "r":
+                    action = self.order_service_action()
+                #prentar þá út main menu annar heldur áfram að halda áfram að prenta út
+                #menu fyrir 1
+                location_flag = "Main menu"
             elif action == 2:
                 pass
             elif action == 3:
@@ -28,10 +38,49 @@ class UserInterface:
             elif action == 5:
                 pass
 
+    def order_service_action(self, location_flag):
+        self.print_choices(location_flag)
+        action = self.get_action()
+        if action == 1:
+            self.__order_service.add_new_order(new_order)
+        elif:
+            pass
+        return action
+
+    def print_choices(self, location_flag):
+        #main screent print options
+        if location_flag == "Main menu":
+            print("------------Main screen options------------")
+            print("Please chose one of the following options:")
+            print("1. Orders")
+            print("2. Customers")
+            print("3. Payments")
+            print("4. Employees")
+            print("5. Cars")
+            print("Press q to quit")
+
+        #order service options
+        elif location_flag == "Order menu":
+            print("------------Order menu options------------")
+            print("Please chose one of the following options:")
+            print("1. Place a new order")
+            print("Press r to return to main menu")
+        
+        #customer service options
+        elif:
+
+        #payment service options
+        elif:
+
+        #employee service options
+        elif:
+
+        #car service options
+        elif:
 
 
     def get_action(self):
-        correct_input_list = [1, 2, 3, 4, 5, "q"]
+        correct_input_list = [1, 2, 3, 4, 5, "q", "r"]
         action = input("Please choose an option: ").lower()
 
         done = False
@@ -47,21 +96,11 @@ class UserInterface:
 
         return action
 
-    def print_choices(self, action):
-        if action == "":
-            print("")
-        elif:
-        
-        elif:
-
-        else:
-
     #kannski mögulega prenta út fancy interface
     def __str__(self):
         pass
 
-    def order_service_action(self):
-        pass
+
 
     def customer_service_action(self):
         pass
