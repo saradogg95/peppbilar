02cd474f0ceaed44542e672c03219e9edef75b4e
Valtyrs <valtyr321@gmail.com>
2018-12-07 13:52:15 +0000

    Version 10

--- services/PaymentServices.py
+++ services/PaymentServices.py
@@ -1,5 +1,18 @@
+from models.Payment import Payment
+from repositories.PaymentRepo import PaymentRepository
 
+class PaymentServices():
 
-class PaymentServices:
+        def __init__(self):
+                self.__repository = PaymentRepository()
+                self.__payments = []
 
-    pass
+        def get_payment_info(self, order_id):
+                get_payment_info = self.__repository.get_payment_info(order_id)
+                if get_payment_info != None:
+                        return get_payment_info
+                return "No payment information for order {} found".format(order_id)
+
+        def add_payment(self, new_payment):
+                #self.__repository.add_payment(new_payment)
+                pass
