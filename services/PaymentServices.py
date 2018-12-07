8796d64eb3530e1b03ee8dc39fab508915a9918c
saradogg95 <saradoggsigurdardottir@gmail.com>
2018-12-07 09:00:09 +0000

    added PaymentServices draft

--- services/PaymentServices.py
+++ services/PaymentServices.py
@@ -2,5 +2,21 @@
 #get_payment_info(order_id): list
 #add_payment(): none
 
-#def add_order(self, new_order):
-        #self.__repository.add_order(new_order)
+from models.Payment import Payment
+from repositories.PaymentRepo import PaymentRepository
+
+class PaymentServices():
+
+        def __init__(self):
+                self.__repository = PaymentRepository()
+                self.__payments = []
+
+        def get_payment_info(self, order_id):
+                get_payment_info = self.__repository.get_payment_info(order_id)
+                if get_payment_info != None:
+                        return get_payment_info
+                return "No payment information for order {} found".format(order_id)
+
+        def add_payment(self, new_payment):
+                #self.__repository.add_payment(new_payment)
+                pass
