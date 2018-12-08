9f636d5a3513f83e9606114905ef2e53996ffa63
Unnar Fridrik Sigurdsson <sveppagreifinn@gmail.com>
2018-12-08 19:46:03 +0000

    Changed to work with db as a single list in data layer

--- services/CustomerServices.py
+++ services/CustomerServices.py
@@ -1,25 +1,35 @@
 from models.Customer import Customer
-from repositories.CustomerRepo import customerRepository
+from repositories.CustomerRepo import CustomerRepository
 
 class CustomerServices:
-    
     def __init__(self):
-        
-        self.__repository = customerRepository()
-        self.__customers = []
-    
-    def add_customer(self, new_customer):
-        self.__repository.add_customer(new_customer)
+        self.__customer_db = CustomerRepository()
 
-    def change_customer(self, new_customer):
-        """ Takes in a customer and updates their information """
-        new_list = []
-        if len(self.__customers) == 0:
-            self.__customers = self.__repository.get_all_customers()
-        for customer in self.__customers:
-            if customer.get_customer_ID() == new_customer.get_customer_ID():
-                customer.append(new_customer)
-            else:
-                customer.append(customer)
-        self.__customers = new_list
-        return self.__customers
+    def add_customer(self, new_customer):
+        self.__customer_db.add_customer(new_customer)
+
+    def change_customer(self, customer_id, new_customer):
+        """ Takes in a customer id and a customer and replaces the old customer with the new customer. If no customer with the customer id is found the new customer added as a customer. """
+        for index, customer in enumerate(self.__customer_db.get_all_customers()):
+            if customer.get_customer_id() == customer_id:
+                self.__customer_db.delete_customer(index)
+        self.__customer_db.add_customer(new_customer)
+    
+    def get_customer(self, customer_id):
+        """ Takes in a customer id, looks it up in the database and returns the customer. If no customer is found a string is returned. """
+        for customer in self.__customer_db.get_all_customers():
+            if customer.get_customer_id() == customer_id:
+                return customer
+        return "No customer with customer id {} found.".format(customer_id)
+
+    def delete_customer(self, customer_id):
+        """ Takes in a customer id and deletes it from the database. Returns a string stating whether the customer was deleted or not. """
+        for index, customer in self.__customer_db.get_all_customers():
+            if customer.get_customer_id() == customer_id:
+                self.__customer_db.delete_customer(index)
+                return "Customer with customer id {} was deleted.".format(customer_id)
+        return "No customer with customer id {} was found.".format(customer_id)
+
+    def write_db_to_file(self):
+        """ Writes the database to a file. Call this before the program ends to save the state of the database. """
+        self.__customer_db.write_customer_db_to_file()
