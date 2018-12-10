16999baa8d4aa9e605ec889887df24653e96e56d
saradogg95 <42866182+saradogg95@users.noreply.github.com>
2018-12-10 16:39:53 +0000

    Fixed spaces

--- models/Payment.py
+++ models/Payment.py
@@ -7,21 +7,27 @@ class Payment:
         self.__additional_cost = additional_cost
         self.__orders_id = orders_id
         
+        
     def __str__(self):
         return "{} {} {} {}".format(self.__id, self.__basic_price, 
-                                       self.__additional_cost, self.__orders_id)
+                                    self.__additional_cost, 
+                                    self.__orders_id)
 
     def get_id(self):
         return self.__id
 
+    
     def get_basic_price(self):
         return self.__basic_price
 
+    
     def get_additional_cost(self):
         return self.__additional_cost
 
+    
     def get_total_cost(self):
         return self.__additional_cost
 
+    
     def get_orders_id(self):
         return self.__orders_id
