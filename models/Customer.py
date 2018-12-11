7c546b7a942ed8a3cc2cb7267393c83aaf79ed92
Unnar Fridrik Sigurdsson <unnar18@ru.is>
2018-12-11 13:46:07 +0000

    Minor changes

--- models/Customer.py
+++ models/Customer.py
@@ -56,6 +56,5 @@ class Customer:
     def set_surname(self, surname):
         self.__surname = surname
     
-    
     def set_credit_card_no(self, credit_card_no):
         self.__credit_card_no = credit_card_no
