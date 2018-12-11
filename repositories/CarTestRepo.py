55a3d1d69283830ae51551592c2cb0267e3a61b1
saradogg95 <42866182+saradogg95@users.noreply.github.com>
2018-12-11 12:36:06 +0000

    Commented out one line

--- repositories/CarTestRepo.py
+++ repositories/CarTestRepo.py
@@ -7,7 +7,7 @@ def main():
     car_db = CarRepository()
     car_service = CarServices()
     car = car_db.get_cars()
-    cars = car_service.get_available_cars()
+    #cars = car_service.get_available_cars()
     print(cars)
 
 
