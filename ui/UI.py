71195e4a43cdd9399b505b4924d6ca1acaff424d
throstur15@ru.is <throstur15@ru.is>
2018-12-11 11:39:22 +0000

    Aðgerðir sem voru í OrderService er snýr að kostnað, voru færðar upp um lag, til að tryggja að ekki væri ósamræmi í undirliggjandi gögnum.

--- ui/UI.py
+++ ui/UI.py
@@ -10,6 +10,8 @@ from models.Employee import Employee
 from models.Payment import Payment
 from models.Customer import Customer
 
+from datetime import datetime
+
 
 class UserInterface:
 
@@ -177,6 +179,47 @@ class UserInterface:
                 done = True
 
         return action
+
+
+    def get_additional_insuarance_cost(self, order_id):
+        """ Takes in an order id and gets that order from the database and calculates the cost of additional insurance"""        
+        for order in self.__order_db.get_all_orders():
+            if order.get_order_id() == order_id:           
+                #Check if additional inusarance was ordered
+                if order.get_additional_insurance() == "TRUE":                
+                    #From the order object, we obtain the registration number for the car and send it into get_car_by_regnum to get car category price
+                    car = self.__car_services.get_car(order.get_car_id())                 
+                    #The cost of insurance is the 75% of the price of a days rental
+                    return int(car[0].get_category_price()) * float(0.75)
+                else:
+                   return None 
+            else:
+                return "No order with order number {} found.".format(order_id)
+    
+    def get_additional_cost_extra_millage(self, order_id):
+        """ Takes in an order id and gets that order from the database and calculates the cost of additional insurance"""        
+        for order in self.__order_db.get_all_orders():
+            if order.get_order_id() == order_id:                      
+                #From the order object, we obtain the registration number for the car and send it into get_car_by_regnum to get car category price
+                car = self.__car_services.get_car(order.get_car_id())                 
+                #The cost of additional millage over 100km is 1% of daily rental cost
+                return int(car[0].get_category_price()) * 0.01
+            else:
+                return "No order with order number {} found.".format(order_id)
+
+    def get_cost_without_additions(self, order_id):
+        """ Takes in an order id and gets that order from the database and calculates the cost without additions"""        
+        for order in self.__order_db.get_all_orders():
+            if order.get_order_id() == order_id:  
+                #We need the number of days the car is being rent for to calculat the total cost
+                start_date = datetime.strptime(order.get_rent_date_from(), "%d/%m/%Y")
+                end_date = datetime.strptime(order.get_rent_date_to(), "%d/%m/%Y")           
+                number_of_days = abs((end_date-start_date).days)
+                #From the order object, we obtain the registration number for the car and send it into get_car_by_regnum to get car category price
+                car = self.__car_services.get_car(order.get_car_id())
+                return int(car[0].get_category_price()) * number_of_days                     
+            else:
+                return "No order with order number {} found.".format(order_id)
 
     #kannski mögulega prenta út fancy interface
     def __str__(self):
