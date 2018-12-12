96e1d7f358ac4110e822777997c2771cbf246f8f
Unnar Fridrik Sigurdsson <unnar18@ru.is>
2018-12-12 16:48:33 +0000

    Added new methods

--- models/Order.py
+++ models/Order.py
@@ -2,7 +2,7 @@ class Order:
     
     def __init__(self, order_id="", order_date="", rent_date_from="", 
                  rent_date_to="",insurance_with_credit_card = "", 
-                 mileage_out="", mileage_in="",
+                 bought_km = 100,
                  additional_insurance=False,
                  customer_id=0, car_id=0):
         self.__order_id = order_id
@@ -10,51 +10,46 @@ class Order:
         self.__rent_date_from = rent_date_from
         self.__rent_date_to = rent_date_to
         self.__insurance_with_credit_card = insurance_with_credit_card
-        self.__mileage_out = mileage_out
-        self.__mileage_in = mileage_in
         self.__additional_insurance = additional_insurance
         self.__customer_id = customer_id
         self.__car_id = car_id
+        self.__bought_km = bought_km
 
 
     def __str__(self):
         return "{} {} {} {} {} {} {} {} {} {}".format(self.__order_id, self.__order_date, 
                                           self.__rent_date_from, self.__rent_date_to,                                         
                                           self.__insurance_with_credit_card,
-                                          self.__mileage_out, self.__mileage_in,
+                                          self.__bought_km
                                           self.__additional_insurance,
                                           self.__customer_id, self.__car_id)
 
     def get_order_id(self):
         return self.__order_id
 
-    
     def get_order_date(self):
         return self.__order_date
 
-    
     def get_rent_date_from(self):
         return self.__rent_date_from
 
-    
     def get_rent_date_to(self):
         return self.__rent_date_to
-        
-        
+
     def get_additional_insurance(self):
         return self.__additional_insurance
-
 
     def get_insurance_with_credit_card(self):
         return self.__insurance_with_credit_card
 
-    
     def get_customer_id(self):
         return self.__customer_id
 
-    
     def get_car_id(self):
         return self.__car_id
+
+    def get_bought_km(self):
+        return self.__bought_km
 
     def set_order_id(self, order_id):
         self.__order_id = order_id
@@ -70,15 +65,6 @@ class Order:
 
     def set_additional_insurance(self, additional_insurance):
         self.__additional_insurance = additional_insurance
-
-
-    def get_mileage_out(self):
-        return self.__mileage_out 
-    
-
-    def get_mileage_in(self):
-        return self.__mileage_in 
-
 
     def set_mileage_in(self, mileage):
         self.__mileage_in = mileage
